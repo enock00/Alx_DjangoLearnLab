e23e5971bfb79c41eb66203f3371330c6dff65be
Enock <enockzy54@gmail.com>
2025-07-27 12:03:20 +0300

    Django models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,16 +4,45 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Profile(models.Model):
     author = models.OneToOneField(Author, on_delete=models.CASCADE)
     bio = models.TextField()
 
-# ForeignKey: Book belongs to one Author
+    def __str__(self):
+        return f"{self.author.name}'s Profile"
+
+# Library model
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+# OneToOne: Librarian for each Library
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# ForeignKey: Book belongs to one Author and one Library
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
-# ManyToMany: Book can have multiple categories, and category can include many books
+    def __str__(self):
+        return self.title
+
+# ManyToMany: Book can belong to many categories, and vice versa
 class Category(models.Model):
     name = models.CharField(max_length=50)
     books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
