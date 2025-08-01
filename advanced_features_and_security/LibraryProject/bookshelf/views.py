0b43fc369ac22552c935c514091f45b949c87eba
Enock <enockzy54@gmail.com>
2025-08-01 21:00:38 +0300

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/view_books.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    # Your book creation logic here
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Edit logic
+    pass
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,38 +1,45 @@
 from django.db import models
-from django.conf import settings
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if not extra_fields.get('is_staff') or not extra_fields.get('is_superuser'):
-            raise ValueError('Superuser must have is_staff=True and is_superuser=True')
-        
-        return self.create_user(username, email, password, **extra_fields)
+from django.conf import settings
 
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    objects = CustomUserManager()
+# Use the custom user model from bookshelf
+User = get_user_model()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.username
+        return self.name
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.PositiveIntegerField()
+    libraries = models.ManyToManyField(Library, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -47,36 +54,11 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-@receiver(post_save, sender=CustomUser)
+
+# Signal to auto-create UserProfile on registration
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    publication_year = models.PositiveIntegerField()
-    libraries = models.ManyToManyField('Library', related_name='books')
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can change book"),
-            ("can_delete_book", "Can delete book"),
-        ]
 
