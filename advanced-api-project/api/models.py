54341a02d289516ac5fc21ec900f7f535349e5c2
Enock <enockzy54@gmail.com>
2025-08-08 18:43:29 +0300

    Django Project with Custom Serializers in Django REST Framework

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from django.db import models
+from datetime import datetime
+
+class Author(models.Model):
+   
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+   
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/list_books.html', {'books': books})
+
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_books(request):
     books = Book.objects.all()
