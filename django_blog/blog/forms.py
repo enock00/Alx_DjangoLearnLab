3c14fc48e77091372ea506e33e08de12f34812c2
Enock <enockzy54@gmail.com>
2025-08-19 09:09:08 +0300

    Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .models import Post, Tag
 from django import forms
 from .models import Comment
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -23,6 +24,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            "tags": TagWidget(), 
+        }
+        
 
     def save(self, commit=True):
         post = super().save(commit=False)
