79411d5fcdd585d471432c040bb9581a6cfa4ec7
Enock <enockzy54@gmail.com>
2025-08-23 05:41:32 +0300

    User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,19 +12,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    
-    password = serializers.CharField(write_only=True)
+
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-    
+        
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
         )
-        Token.objects.create(user=user) 
+        Token.objects.create(user=user)
         return user
